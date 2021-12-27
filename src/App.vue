cb2b22eb1ca415d593b6f0284926b51298971a42
chen <158874645@qq.com>
2021-12-28 00:46:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <tableTest/>
+</template>
+
+<script>
+import tableTest from './components/tableTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    tableTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/tableTest.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div>
+    <table></table>
+
+    <table></table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tableTest",
+  data() {
+    return {};
+  },
+  beforeMount() {},
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-around;
+}
+table {
+  border-collapse: collapse;
+}
+.is-next {
+  background: #f9f5e6;
+}
+.is-grace {
+  background: #f2e1a4;
+}
+.is-grace.is-top {
+  background: #ef9999;
+  color: #fff;
+}
+td,
+th {
+  line-height: 40px;
+  border: 1px solid #ccc;
+  padding: 0 10px;
+}
+</style>
