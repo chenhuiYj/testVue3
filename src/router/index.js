3c1fd58fb49ec47a6f2f1c36cff5ef29f47479fa
chen <158874645@qq.com>
2022-04-13 23:55:27 +0800

    新加答题卡页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 const home = () => import("../page/tableTest")
 const game = () => import("../page/game1")
+const answerSheet = () => import("../page/answerSheet")
 
 const routes = [
     { path: "/", redirect: "/home" },
@@ -13,6 +14,11 @@ const routes = [
         path: "/game",
         name: "game",
         component: game
+    },
+    {
+        path: "/answer-sheet",
+        name: "answerSheet",
+        component: answerSheet
     }
 ]
 
