22af1bb5653c4df963de239d23c20f93361d3de3
chen <158874645@qq.com>
2022-04-18 01:19:40 +0800

    动态编辑

--- src/page/answerSheet.vue
+++ src/page/answerSheet.vue
@@ -3,49 +3,73 @@
   <div class="mian">
     <div class="button-number main-box">
       <div class="number-box">
-        <button v-for="(item, index) in questionList" :key="item.id">
+        <button
+          v-for="(item, index) in questionList"
+          :key="item.id"
+          @click="handleSelect(item, index)"
+        >
           {{ index + 1 }}
         </button>
       </div>
       <div class="button-box">
-        <button>添加选择题</button>
-        <button>添加填空题</button>
-        <button>添加问答题</button>
+        <button @click="hanldeAddQuestion('choose')">添加选择题</button>
+        <button @click="hanldeAddQuestion('fill')">添加填空题</button>
+        <button @click="hanldeAddQuestion('ask')">添加问答题</button>
       </div>
     </div>
-    <div class="question-info main-box">
-      <div
-        v-for="(item, index) in questionList"
-        :key="item.id"
-        class="question-box"
-      >
-        <div class="question-title">{{ index + 1 }}.{{ item.content }}</div>
-        <div class="answer-list" v-if="item.type === 'choose'">
+    <div class="question-info main-box" v-if="curQuestion.id">
+      <div class="question-box">
+        <div class="question-title">
+          {{ curIndex + 1 }}.<input v-model="curQuestion.content" />
+        </div>
+        <div class="answer-list" v-if="curQuestion.type === 'choose'">
           <ol type="A">
-            <li v-for="answer in item.answerList" :key="answer">
-              {{ answer }}
+            <li v-for="(answer, index) in curQuestion.answerList" :key="index">
+              {{ numberMap[index] }}.
+              <input v-model="curQuestion.answerList[index]" />
             </li>
+            <button @click="hanldeAddAnswer">添加选项</button>
           </ol>
         </div>
-        <div class="answer-list" v-if="item.type === 'fill'">
+        <div class="answer-list" v-if="curQuestion.type === 'fill'">
           <ul type="A">
-            <li v-for="grace in item.grace" :key="grace">
+            <li v-for="grace in curQuestion.grace" :key="grace">
               <input />
             </li>
           </ul>
         </div>
-        <div class="answer-list" v-if="item.type === 'ask'">
+        <div class="answer-list" v-if="curQuestion.type === 'ask'">
           <textarea />
         </div>
       </div>
     </div>
-    <div class="button-other main-box">
+    <div class="button-other main-box" v-if="curQuestion.id">
       <ul>
-        <li>分值: 5分</li>
-        <li>正确答案:A</li>
-        <li>序号:3</li>
+        <li>分值: <input v-model="curQuestion.grace" />分</li>
+        <li>
+          正确答案:
+          <span
+            class="answer-item"
+            v-for="item in curQuestion.answerList"
+            :key="item"
+          >
+            <input
+              type="checkbox"
+              :value="item"
+              @change="handleSelectAnswer"
+            />{{ item }}
+          </span>
+        </li>
+        <li>
+          序号:
+          <select>
+            <option v-for="(item, index) in questionList" :key="item">
+              {{ index + 1 }}
+            </option>
+          </select>
+        </li>
       </ul>
-      <button>保存</button>
+      <button @click="handleSave">保存</button>
     </div>
   </div>
 </template>
@@ -55,48 +79,111 @@ export default {
   name: "AnswerSheet",
   data() {
     return {
-      questionList: [
-        {
-          id: 1,
-          content: "这道题该选什么？",
-          type: "choose",
-          answer: "A",
-          answerList: ["选择A", "选择B", "选择C", "选择D"],
-          grace: 5,
-        },
-        {
-          id: 2,
-          content: "这道题该选什么？",
-          type: "choose",
-          answer: "A,B",
-          answerList: ["选择A", "选择B", "选择C", "选择D"],
-          grace: 5,
-        },
-        {
-          id: 3,
-          content: "这是一道填空题，首先是___，然后是___",
-          type: "fill",
-          answer: "A/*/B",
-          grace: [2, 3],
-        },
-        {
-          id: 4,
-          content: "这是一道简单题",
-          type: "fill",
-          answer: "A",
-          grace: [5],
-        },
-        {
-          id: 5,
-          content: "这是一道问答题",
-          type: "ask",
-          answer: "",
-          grace: 15,
-        },
+      questionAttr: {
+        type: "question", //question：逐一填写，list：全部一起填写
+        realTimeGrace: true, //是否实时显示分数
+        showComfirm: true, //是否显示确认答案页面
+      },
+      questionList: [],
+      curQuestion: {},
+      curIndex: null,
+      numberMap: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
       ],
     };
   },
   beforeMount() {},
+  methods: {
+    hanldeAddQuestion(type) {
+      let obj = {};
+      let id = this.questionList.length + 1;
+      switch (type) {
+        case "choose":
+          obj = {
+            id,
+            content: "",
+            type: "choose",
+            answer: [],
+            answerList: [],
+            graceMapAnswer: [], //答对部分选项对应分数
+            grace: 0,
+          };
+          break;
+        case "fill":
+          obj = {
+            id,
+            content: "",
+            type: "fill",
+            answer: [],
+            graceMapAnswer: [], //每一处对应得分
+            grace: 5,
+          };
+          break;
+        case "ask":
+          obj = {
+            id,
+            content: "",
+            type: "ask",
+            answer: "",
+            grace: 5,
+          };
+          break;
+      }
+      this.questionList.push(obj);
+    },
+    handleSelect(item, index) {
+      let _item = JSON.parse(JSON.stringify(item));
+      this.curQuestion = _item;
+      this.curIndex = index;
+    },
+    hanldeAddAnswer() {
+      this.curQuestion.answerList.push("");
+    },
+    handleSave() {
+      debugger;
+      //调整答案顺序
+      let _answer = this.curQuestion.answerList.filter(
+        (item) => this.curQuestion.answer.indexOf(item) !== -1
+      );
+      this.curQuestion.answer = _answer;
+      this.questionList[this.curIndex] = this.curQuestion;
+    },
+    handleSelectAnswer(e) {
+      let _index = this.curQuestion.answerList.findIndex(
+        (item) => item === e.target.value
+      );
+      if (e.target.checked) {
+        this.curQuestion.answer.push(e.target.value);
+      } else {
+        this.curQuestion.answer.splice(_index, 1);
+      }
+    },
+  },
 };
 </script>
 
@@ -127,5 +214,8 @@ export default {
 }
 .question-box {
   margin-bottom: 20px;
+}
+.answer-item {
+  margin-right: 10px;
 }
 </style>
