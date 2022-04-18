5c77ec2fcbc939e716aeb10d193a399968dea114
chen <158874645@qq.com>
2022-04-19 00:45:03 +0800

    解决sass版本冲突

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,22 @@
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = {
     pluginOptions: {
         "style-resources-loader": {
             preProcessor: 'scss',
             patterns: []
         }
+    },
+    configureWebpack: {
+        plugins: [
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
     }
 }
